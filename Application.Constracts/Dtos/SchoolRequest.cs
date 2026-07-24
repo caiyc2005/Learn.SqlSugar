2cb3a867db74e89409034b5e67d24710785e5688
永超 蔡 <2673439720@qq.com>
2026-07-24 15:06:33 +0800

    学校DTO与学校服务搭建

---
+++ Application.Constracts/Dtos/SchoolRequest.cs
@@ -0,0 +1,27 @@
+﻿using SqlSugarCoreExtra.Furion.Component.ServiceExts.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Application.Constracts.Dtos
+{
+    public class CreateOrUpdateSchoolRequest : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            return [];
+            //throw new NotImplementedException();
+        }
+    }
+
+    public class SchoolResponse : OutputKey<Guid>
+    {
+
+    }
+
+    public class SchoolPageListRequest : PageListInput
+    {
+
+    }
+}
---
+++ Application/Services/SchoolService.cs
@@ -0,0 +1,23 @@
+﻿using Application.Constracts.Dtos;
+using Domain.Entitys;
+using Domain.Shared.Consts;
+using Furion.DynamicApiController;
+using Microsoft.AspNetCore.Mvc;
+using SqlSugarCoreExtra.Furion.Component.Repositorys;
+using SqlSugarCoreExtra.Furion.Component.ServiceExts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services
+{
+    [ApiDescriptionSettings(groups: ApiGroups.Main, Order = 9000, Tag = "学校信息管理")]
+    public class SchoolService : CrudAppService<School, SchoolResponse, SchoolPageListRequest,
+    CreateOrUpdateSchoolRequest, Guid>, IDynamicApiController
+    {
+        public SchoolService(IRepo<School, Guid> repository) : base(repository)
+        {
+
+        }
+    }
+}
