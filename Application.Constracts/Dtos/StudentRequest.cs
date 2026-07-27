06f10168fbb6aa486c9833231b83e58f71efb0ba
Live.403 <2673439720@qq.com>
2026-07-27 15:16:29 +0800

    初始化学生DTO与学生服务

---
+++ Application.Constracts/Dtos/StudentRequest.cs
@@ -0,0 +1,28 @@
+﻿using SqlSugarCoreExtra.Furion.Component.ServiceExts.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Application.Constracts.Dtos
+{
+    public class CreateOrUpdateStudentRequest : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            return [];
+        }
+    }
+
+    public class StudentResponse
+    {
+
+
+    }
+
+    public class StudentPageListRequest : PageListInput
+    {
+
+    }
+
+}
---
+++ Application/Services/StudentService.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entitys;
+using Domain.Shared.Consts;
+using Furion.DynamicApiController;
+using Microsoft.AspNetCore.Mvc;
+using SqlSugarCoreExtra.Furion.Component.Repositorys;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services
+{
+    [ApiDescriptionSettings(groups: ApiGroups.Main, Order = 4000, Tag = "学生信息管理")]
+    public class StudentService CrudAppService<Student, StudentResponse, StudentPageListRequest,
+    CreateOrUpdateStudentRequest, Guid>, IDynamicApiController
+    {
+        public StudentService(IRepo<Student, Guid> repository) : base(repository)
+    {
+
+    }
+}
+}
