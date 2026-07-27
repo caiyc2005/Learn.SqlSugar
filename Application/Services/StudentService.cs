4c46cbcdb90c9eb2b9b90211c6c50e2d1a17a28a
Live.403 <2673439720@qq.com>
2026-07-27 15:48:50 +0800

    改

--- Application/Services/StudentService.cs
+++ Application/Services/StudentService.cs
@@ -1,8 +1,10 @@
-﻿using Domain.Entitys;
+﻿using Application.Constracts.Dtos;
+using Domain.Entitys;
 using Domain.Shared.Consts;
 using Furion.DynamicApiController;
 using Microsoft.AspNetCore.Mvc;
 using SqlSugarCoreExtra.Furion.Component.Repositorys;
+using SqlSugarCoreExtra.Furion.Component.ServiceExts;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,12 +12,12 @@ using System.Text;
 namespace Application.Services
 {
     [ApiDescriptionSettings(groups: ApiGroups.Main, Order = 4000, Tag = "学生信息管理")]
-    public class StudentService CrudAppService<Student, StudentResponse, StudentPageListRequest,
+    public class StudentService : CrudAppService<Student, StudentResponse, StudentPageListRequest,
     CreateOrUpdateStudentRequest, Guid>, IDynamicApiController
     {
         public StudentService(IRepo<Student, Guid> repository) : base(repository)
-    {
+        {
 
+        }
     }
-}
 }
