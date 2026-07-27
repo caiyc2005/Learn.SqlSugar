1b9ee24524313c56c4673755f81157102d2710f5
Live.403 <2673439720@qq.com>
2026-07-27 15:33:21 +0800

    初始化Parents的DTO

---
+++ Application.Constracts/Dtos/ParentRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Application.Constracts.Dtos
+{
+    public class CreateOrUpdateParentRequest : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            return [];
+            //throw new NotImplementedException();
+        }
+
+        public class SchoolResponse
+        {
+
+
+        }
+
+        public class SchoolPageListRequest
+        {
+
+        }
+    }
+}
